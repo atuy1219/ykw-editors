1f9a9c8e4ddc0cd2b97aad54df6fa75f6a15f1b7
Atuy <130917473+atuy1219@users.noreply.github.com>
2026-08-09 21:50:07 +0900

    Fix Qt resource linkage in B1 plugin

--- android/plugins/ywb1_plugin.cpp
+++ android/plugins/ywb1_plugin.cpp
@@ -8,16 +8,26 @@
 
 namespace {
 QTranslator *g_translator = nullptr;
+
+void activateResources()
+{
+    Q_INIT_RESOURCE(resources);
+}
+
+void deactivateResources()
+{
+    Q_CLEANUP_RESOURCE(resources);
+}
 }
 
 extern "C" Q_DECL_EXPORT void ykw_editor_b1_activate()
 {
-    Q_INIT_RESOURCE(resources);
+    activateResources();
     if (!g_translator) {
         g_translator = new QTranslator(qApp);
         if (!g_translator->load(QLocale::system(), QStringLiteral("app"), QStringLiteral("_"),
                                 QStringLiteral(":/translations/translations"), QStringLiteral(".qm"))) {
-            g_translator->load(QStringLiteral(":/translations/translations/app_en.qm"));
+            (void)g_translator->load(QStringLiteral(":/translations/translations/app_en.qm"));
         }
         qApp->installTranslator(g_translator);
     }
@@ -30,7 +40,7 @@ extern "C" Q_DECL_EXPORT void ykw_editor_b1_deactivate()
         delete g_translator;
         g_translator = nullptr;
     }
-    Q_CLEANUP_RESOURCE(resources);
+    deactivateResources();
 }
 
 extern "C" Q_DECL_EXPORT QWidget *ykw_editor_b1_create()
