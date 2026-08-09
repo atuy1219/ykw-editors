56cb168172d91b1188dfb84dd236399d1a02935a
Atuy <130917473+atuy1219@users.noreply.github.com>
2026-08-09 20:39:39 +0900

    Wrap Yo-kai Watch 1 editor as plugin

---
+++ android/plugins/yw1_plugin.cpp
@@ -0,0 +1,39 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include <QLocale>
+#include <QResource>
+#include <QTranslator>
+#include <QWidget>
+
+namespace {
+QTranslator *g_translator = nullptr;
+}
+
+extern "C" Q_DECL_EXPORT void ykw_editor1_activate()
+{
+    Q_INIT_RESOURCE(resources);
+    if (!g_translator) {
+        g_translator = new QTranslator(qApp);
+        const QString resource = QStringLiteral(":/translations/translations/qt_%1.qm")
+                                     .arg(QLocale::system().name());
+        if (g_translator->load(resource)) {
+            qApp->installTranslator(g_translator);
+        }
+    }
+}
+
+extern "C" Q_DECL_EXPORT void ykw_editor1_deactivate()
+{
+    if (g_translator) {
+        qApp->removeTranslator(g_translator);
+        delete g_translator;
+        g_translator = nullptr;
+    }
+    Q_CLEANUP_RESOURCE(resources);
+}
+
+extern "C" Q_DECL_EXPORT QWidget *ykw_editor1_create()
+{
+    return new MainWindow();
+}
